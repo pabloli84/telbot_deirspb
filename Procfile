2c412830e34c051bfb7db0d0e05e01d5c9b03a77
pabloli84 <pabloli@yandex.ru>
2018-03-29 10:41:51 +0300

    Revert "Telbot on heroku"

--- Procfile
+++
@@ -1,1 +0,0 @@
-bot: node telbot.js
--- telbot.js
+++ telbot.js
@@ -5,9 +5,9 @@ let botOptions = {
 
 let fs = require('fs');
 
-//let content = fs.readFileSync('./config.json');
-//let config = JSON.parse(content);
-let token = process.env.API_TOKEN;
+let content = fs.readFileSync('./config.json');
+let config = JSON.parse(content);
+let token = config.ApiToken;
 const paymentMethodsFile = "./paymentMethods.json";
 const helpFile = "./help.md";
 
