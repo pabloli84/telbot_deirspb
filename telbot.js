9e8eeca878be570b1c0e44ba15fe2aa0527610a3
Pavel Lipatov <pavel.lipatov@nordigy.ru>
2018-03-29 16:42:27 +0300

    webhooks fix

--- telbot.js
+++ telbot.js
@@ -28,16 +28,16 @@ const linkCourse34Paper = "https://www.ozon.ru/context/detail/id/143653923/";
 
 let bot = new TelegramBot(token, botOptions);
 
-bot.setWebHook(`{$url}/bot$API_TOKEN`);
-
+bot.setWebHook(`${url}/bot${token}`);
+/*
 bot.getMe().then(function(me)
 		 {
 		     console.log('Hello! My name is %s!', me.first_name);
 		     console.log('My id is %s.', me.id);
 		     console.log('And my username is @%s.', me.username);
 		 });
-
-bot.on('text', function(msg)
+*/
+bot.on('message', function(msg)
        {
 	   let messageChatId = msg.chat.id;
 	   let messageText = msg.text.split(" ");
